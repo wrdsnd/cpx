e7d641e053765a3e77e37f756b5fa1b32635a193
Alisa <alice.dreamlight@gmail.com>
2022-04-29 23:17:54 +0200

    feature: delay loading indicator rendering

--- client/components/Status.tsx
+++ client/components/Status.tsx
@@ -1,5 +1,7 @@
 import { Text } from '@chakra-ui/react'
 import { keyframes } from '@emotion/react'
+import { useState } from 'react'
+import { useTimeoutFn } from 'react-use'
 
 const spin = keyframes`
     from {
@@ -10,15 +12,25 @@ const spin = keyframes`
     }
 `
 
-export const Loading = () => (
-  <Text
-    p={4}
-    display="inline-block"
-    sx={{ animation: `${spin} 1s ease infinite` }}
-  >
-    (ಠ.ಠ)
-  </Text>
-)
+export const Loading = () => {
+  const [isVisible, setIsVisible] = useState(false)
+  useTimeoutFn(() => setIsVisible(true), 500)
+  console.log(isVisible)
+
+  if (!isVisible) {
+    return <></>
+  }
+
+  return (
+    <Text
+      p={4}
+      display="inline-block"
+      sx={{ animation: `${spin} 1s ease infinite` }}
+    >
+      (ಠ.ಠ)
+    </Text>
+  )
+}
 
 export const Failed = () => <Text p={4}>(ノ°Д°）ノ︵ ┻━┻</Text>
 
