02d46fa7f66fa389bc760126a9d571dba2506286
Alisa <alice.dreamlight@gmail.com>
2022-04-29 23:17:19 +0200

    fix: twitter url parser regexx

--- client/utils/sources.ts
+++ client/utils/sources.ts
@@ -7,7 +7,8 @@ export const createTweetUrl = ({
 }) => `https://twitter.com/${username}/status/${id}`
 
 export const parseTweetId = (url: string) => {
-  const regExp = /^https?:\/\/twitter\.com\/(?:#!\/)?(\w+)\/status(?:es)?\/(\d+)(?:\/.*)?$/
+  const regExp =
+    /^https?:\/\/twitter\.com\/(?:#!\/)?(\w+)\/status(?:es)?\/(\d+)(?:\?.*)?$/
   const match = regExp.exec(url)
   if (match && match[2]) {
     return match[2]
