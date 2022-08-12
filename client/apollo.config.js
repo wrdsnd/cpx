c0506e49f79b4607eb8095149f57ed13d2bcc1af
Alisa <alice.dreamlight@gmail.com>
2022-08-12 20:39:52 +0200

    chore: apollo.config.js use local schema

--- client/apollo.config.js
+++ client/apollo.config.js
@@ -1,12 +1,10 @@
-require('dotenv').config()
-
 module.exports = {
   client: {
     service: {
       name: 'cutepix',
-      url: `${process.env.NEXT_PUBLIC_API_URL}/graphql`,
+      localSchemaFile: './server/src/schema.graphql'
     },
-    includes: ['./client/pages/**/*.{ts,tsx}', './client/components/**/*.{ts,tsx}'],
+    includes: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}', './providers/**/*.{ts,tsx}'],
     excludes: ['**/node_modules', '**/__tests__/**/*'],
     skipSSLValidation: true,
   },
