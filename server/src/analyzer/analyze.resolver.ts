a0475b1320e0cf98fba303e82db5bc0983447437
Alisa <alice.dreamlight@gmail.com>
2022-08-14 11:04:23 +0200

    chore: migrated to twitter-api-v2 library and dropped custom code

--- server/src/analyzer/analyze.resolver.ts
+++ server/src/analyzer/analyze.resolver.ts
@@ -1,48 +1,14 @@
-import { map, get } from 'lodash'
 import { Args, Query, Resolver } from '@nestjs/graphql'
 import { TwitterService } from '../twitter/twitter.service'
-import { MediaType, News, TwitterMedia } from 'src/graphql'
+import { News } from '../graphql'
 
 @Resolver('News')
 export class AnalyzeResolver {
   constructor(private twitterService: TwitterService) {}
 
-  tweetToPost = async (status: any): Promise<News> => {
-    const mediaEntities = get(status, 'extended_entities.media')
-
-    const media: TwitterMedia[] = map(
-      mediaEntities,
-      (entity: any): TwitterMedia => {
-        switch (entity['type']) {
-          case 'animated_gif': {
-            return {
-              url: entity['video_info']['variants'][0].url,
-              type: MediaType.VIDEO,
-            }
-          }
-          default: {
-            return {
-              url: entity.media_url_https,
-              type: MediaType.IMAGE,
-            }
-          }
-        }
-      },
-    )
-
-    return {
-      id: status.id_str,
-      user: {
-        name: get(status, 'user.screen_name'),
-      },
-      message: status.text,
-      media,
-    }
-  }
-
   @Query()
   async news(@Args('id') id: string): Promise<News> {
-    const tweet: any = await this.twitterService.show({ id })
-    return this.tweetToPost(tweet.data)
+    const tweet = await this.twitterService.twitterClient.v1.singleTweet(id)
+    return this.twitterService.convertTweetToNews(tweet)
   }
 }
